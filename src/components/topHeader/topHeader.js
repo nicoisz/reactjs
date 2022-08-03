f27e81d13dda9177f3806ef2a499da4d5e47afa8
nicoisz <nicolasignacio.sz@gmail.com>
2022-08-03 01:58:12 -0400

    consuming api, creating pagination, creating cards

---
+++ src/components/topHeader/topHeader.js
@@ -0,0 +1,10 @@
+function TopHeader(){
+    return <div className="Rectangle-2-Copy"><span className="HACKER-NEWS Text-Style">
+        HACKER NEWS
+    </span>
+  </div>;
+}
+
+export default TopHeader;
+
+
